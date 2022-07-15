a336fd19a61466a9f771a62133a04b8505eab3c4
Mahmoud Shawa <moshawa0@gmail.com>
2022-07-14 20:49:42 -0400

    fetch data on mount and randomize users

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const API_USERS_URL = 'https://jsonplaceholder.typicode.com/users'
+  const API_ALBUMS_URL = 'https://jsonplaceholder.typicode.com/albums'
+
+  const [users, setUsers] = useState([])
+
+  const fetchData = async () => {
+    const res = await fetch(API_USERS_URL)
+    let data = await res.json()
+    // figured it makes more sense to cull 3 random users than set random 7 
+    for (let i = 0; i < 3; i++) {
+      const random = Math.floor(Math.random() * (data.length - i)) // decrease max by number of iterations, so we don't get an index outside the new length of array 
+      data.splice(random, 1)
+    }
+    setUsers(data)
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, []) // empty dependency array allows useEffect to run only on component mount (mimic componentDidMount in class components)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {users.map(user => (
+        JSON.stringify(user)
+      ))}
     </div>
   );
 }
